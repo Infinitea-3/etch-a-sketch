0ca8cd06afffbe7b2720c8bde1d228abace30079
Infinitea-3 <rummel.nathan@yahoo.com>
2023-08-23 19:49:30 -0400

    Connected slider with grid (still broken, but less broken)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,12 @@
 const container = document.querySelector('.container');
 
-createGrid=()=>{
-    for (let i = 0; i<256; i++){
+fillGrid=(length)=>{
+    removeAllChildNodes(container);
+    fullSize = length * length;
+    for (let i = 0; i<fullSize; i++){
         const grid = document.createElement('div');
         grid.classList.add('grid');
         container.appendChild(grid);
-        console.log(i);
     }
 }
 
@@ -15,19 +16,13 @@ function removeAllChildNodes(parent){
     }
 }
 
-
 const slider = document.querySelector('.slider');
-const rangeValue = document.querySelector('#rangeValue');
 slider.addEventListener('input',function(){
-    removeAllChildNodes(container);
-    console.log(rangeValue);
-    container.setAttribute('style', `grid-template: repeat(${rangeValue},2fr)/repeat(${rangeValue},2fr);`);
-    for (let i=0; i<rangeValue*rangeValue;i++){
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        container.appendChild(grid);
-        console.log(i);
-    }
+    const rangeValue = document.getElementById('rangeValue');
+    rangeValueText = parseInt(rangeValue.textContent);
+    fillGrid(rangeValueText);
 });
 
-createGrid();
+//Create Initial Grid
+size = 15;
+fillGrid(size);
